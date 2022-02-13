b3bc9bca73c8e5f5848caf0c294bea2602d2885f
Sang <sanghosuh@gmail.com>
2022-02-12 21:54:04 -0800

    footer progress

---
+++ src/components/Footer.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <footer className="section footer">
+      <p className="copyright">
+        copyright &copy; backroads travel tour company <span id="date"></span>.
+        all rights reserved
+      </p>
+    </footer>
+  );
+};
+
+export default Footer;
